38e96bc9772e97b717d6427f25dbc62dfccf80e4
Adrian Loewenberg Casas <Adrian.Loewenberg.Casas@lv1871.de>
2025-02-04 17:34:23 +0100

    Day 6 part 2

--- Day6.cbl
+++ Day6.cbl
@@ -10,7 +10,6 @@
            select input-file assign to "inputDay6.txt"
                line sequential.
                
-      *    select output-file assign to "output.txt" line sequential.
        data division.
        file section.
 
@@ -19,31 +18,57 @@
        78 input-width value 130.
       *78 input-height value 10.
       *78 input-width value 10.
+      
        01 input-line pic X(input-width).
        
-      *fd output-file.
-      *01 out-line pic X(input-width).
-
        working-storage section.
        01 ws-eof pic x(1).
        
        01 result binary-long value 0.
+       01 path-length binary-long value 1.
+       01 cycle-count binary-long value 0.
        
-       01 map-line occurs input-height times indexed by y.
-           05 map pic X occurs input-width times indexed by x.
-               copy 'Day6_MapCells.cpy'.
+       01 map-memory.
+           05 map-line occurs input-height times indexed by y.
+               10 map pic X occurs input-width times indexed by x.
+                   copy 'Day6_MapCells.cpy'.
        
-       01 filler.
+       78 up-flag value B#1000.
+       78 down-flag value B#0100.
+       78 left-flag value B#0010.
+       78 right-flag value B#0001.
+       01 visited-memory.
+           05 filler occurs input-height times.
+               10 cell-visited pic 9 usage comp-5 occurs input-width    
+               times.
+       
+       01 map-memory-copy.
+         05 filler occurs input-height times.
+               10 filler pic X occurs input-width times.
+       
+       01 curr-cell-data.
          05 curr-cell pic X.
            copy 'Day6_MapCells.cpy'.
+         05 visited-curr-cell pic 9 usage comp-5.
          05 curr-y pic 9(3).
          05 curr-x pic 9(3).
+         
+       01 curr-cell-data-copy.
+         05 filler pic X.
+         05 filler pic 9.
+         05 filler pic 9(3).
+         05 filler pic 9(3).
            
-       01 filler.
+       01 next-cell-data.
          05 next-cell pic X.
            copy 'Day6_MapCells.cpy'.
          05 next-y pic 9(3).
          05 next-x pic 9(3).
+       
+       01 next-cell-data-copy.
+         05 filler pic X.
+         05 filler pic 9(3).
+         05 filler pic 9(3).
            
        procedure division.
 
@@ -57,7 +82,6 @@
                        input-width
                            move map(y,x) to next-cell
                            if guard of next-cell
-                               add 1 to result
                                move map(y,x) to curr-cell
                                set curr-y to y
                                set curr-x to x
@@ -68,38 +92,59 @@
                move 'n' to ws-eof
            close input-file.
            
-           perform test after until goal of next-cell
-               perform test after until not wall of next-cell
-      *            perform print-map
+      *    perform part-one.
+           perform part-two.
            
-                   perform set-next-cell
-                   perform process-next-cell
-               end-perform
-           end-perform
-           
-      *    open output output-file
-      *    perform varying y from 1 by 1 until y > input-height
-      *        move map-line(y) to out-line
-      *        write out-line
-      *    end-perform
-      *    close output-file
-
            display result
            goback.
        
+       part-one.
+           perform walk-guard-duty
+           move path-length to result.
+           
+       part-two.
+           perform varying y from 1 by 1 until y > input-height
+               perform varying x from 1 by 1 until x > input-width
+                   if path of map(y,x)
+                       move map-memory to map-memory-copy
+                       move curr-cell-data to curr-cell-data-copy
+                       move next-cell-data to next-cell-data-copy
+                       move 0 to visited-memory
+                       
+                       set artificial-wall of map(y,x) to true
+                       perform walk-guard-duty
+                       
+                       move map-memory-copy to map-memory
+                       move curr-cell-data-copy to curr-cell-data  
+                       move next-cell-data-copy to next-cell-data  
+                   end-if
+               end-perform
+           end-perform
+           move cycle-count to result.
+           
+       walk-guard-duty.    
+           perform test after until goal of next-cell
+               perform set-next-cell
+               perform process-next-cell
+           end-perform.
+           
        set-next-cell.
            evaluate true
                when guard-up of curr-cell 
                    compute next-y = curr-y - 1
+                   set visited-curr-cell to up-flag   
                    move curr-x to next-x
                when guard-down of curr-cell 
                    compute next-y = curr-y + 1
+                   set visited-curr-cell to down-flag                   
                    move curr-x to next-x
                when guard-right of curr-cell 
                    compute next-x = curr-x + 1
+                   set visited-curr-cell to right-flag                  
                    move curr-y to next-y
                when guard-left of curr-cell 
                    compute next-x = curr-x - 1
+                   set visited-curr-cell to left-flag                   
                    move curr-y to next-y
            end-evaluate
            if next-y >= 1 and next-y <= input-height 
@@ -113,9 +158,22 @@
            evaluate true
                when path of next-cell
                    if new-path of next-cell
-                       add 1 to result
+                       add 1 to path-length
                    end-if
+                   
+                   if 
+                     (cell-visited(next-y, next-x) b-and
+                     visited-curr-cell) 
+                   not equal to 0
+                       add 1 to cycle-count
+                       set goal of next-cell to true
+                   end-if
+                   
                    set visited-path of map(curr-y,curr-x) to true
+                   
+                   compute cell-visited(curr-y,curr-x) = 
+                   cell-visited(curr-y,curr-x) b-or visited-curr-cell
+                   
                    move curr-cell to map(next-y, next-x)
                    move next-y to curr-y
                    move next-x to curr-x
--- Day6_MapCells.cpy
+++ Day6_MapCells.cpy
@@ -3,7 +3,8 @@
        88 guard-right value '>'.
        88 guard-down value 'v'.
        88 guard-left value '<'.
-       88 wall value '#'.
+       88 wall values '#' 'O'.
+       88 artificial-wall value 'O'.
        88 path value '.' 'X'.
        88 new-path value '.'.
        88 visited-path value 'X'.
