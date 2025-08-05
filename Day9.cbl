0af711b70b3f88a0e5e78cedee688dfef43a717e
Adrian Loewenberg Casas <Adrian.Loewenberg.Casas@lv1871.de>
2025-08-05 14:56:24 +0200

    Day 9 Part Two.

--- Day9.cbl
+++ Day9.cbl
@@ -23,27 +23,44 @@
        
        01 display-number pic Z(19)9.
        
-       01 current-file binary-long value 1.
+       01 current-file binary-long value 0.
+       01 current-file-size pic 9.
+       
        01 filler pic X value 'N'.
            88 currently-free-space value 'Y' false 'N'.
        
+       01 disk-map.
+         05 map-entry occurs 30000 times indexed by j.
+           10 map-entry-size pic 9 value 0.
+           10 filler pic X value 'Y'.
+               88 map-entry-empty value 'Y' false 'N'.
+           10 map-entry-file-id binary-long.
+         05 last-written-map-entry binary-long.
+      *  When this is set, shift-map can be called, all elements at pos.
+      *  >= insert-index will shift one index to the right. 
+      *  map-entry(insert-index) will be en empty entry of size 0.
+         05 insert-index binary-long.
+           
        01 filler.
-         05 block-id occurs 180000 times indexed by i.
-           10 block-status pic X value 'Y'.
+         05 block-file occurs 180000 times indexed by i.
+           10 filler pic X value 'Y'.
                88 block-empty value 'Y' false 'N'.
            10 block-file-id binary-long.
-       
-       01 last-written-block binary-long.
+         05 last-written-block binary-long.
        
        01 result pic 9(38) value 0.
        01 temp binary-long.
        
-       01 compact-disc-variables.
-         05 right-pointer binary-long.
-         05 left-pointer binary-long.
-           
+       01 compact-disk-fragmentation-variables.
+         05 right-block binary-long.
+         05 left-block binary-long.
+       
+       01 compact-disk-whole-files-variables.
+         05 right-map-entry binary-long.
+         05 left-map-entry binary-long.
+         
        procedure division.
-           set i to 1
+           set i j to 1
            open input input-file
                perform until ws-eof='y'
                read input-file
@@ -57,50 +74,124 @@
                end-perform.
                move 'n' to ws-eof
            close input-file.
-           perform part-one
+           subtract 1 from current-file
+      *    perform part-one
+           perform part-two
            goback.
 
        process-input-number.
            if currently-free-space
-               perform input-number times
-                   set block-empty(i) to true
-                   set i up by 1
-               end-perform
+               set map-entry-empty(j) to true
+               move input-number to map-entry-size(j)
                set currently-free-space to false
            else
-               perform input-number times
-                   set block-empty(i) to false
-                   compute block-file-id(i) = current-file - 1
-                   set i up by 1
-               end-perform
+               set map-entry-empty(j) to false
+               move input-number to map-entry-size(j)
+               move current-file to map-entry-file-id(j)
                add 1 to current-file
                set currently-free-space to true
            end-if
-           set last-written-block to i.
+           set j up by 1
+           set last-written-map-entry to j.
        
+           
        part-one.
+           perform disk-map-to-blocks
            perform compact-disk-fragmentation
            perform checksum
            display result.
            
-       compact-disk-fragmentation.
-           compute right-pointer = last-written-block - 1
-           move 1 to left-pointer
-           perform until right-pointer <= left-pointer
-               if block-empty(right-pointer)
-                   subtract 1 from right-pointer
-               else
-                   if block-empty(left-pointer)
-                       set block-empty(left-pointer) to false
-                       set block-empty(right-pointer) to true
-                       move block-file-id(right-pointer) to 
-                       block-file-id(left-pointer)
+       part-two.
+           perform compact-disk-whole-files
+           perform disk-map-to-blocks
+           perform checksum
+           display result.
+       
+       disk-map-to-blocks.
+           set i to 1
+           perform varying j from 1 by 1 until j >= 
+           last-written-map-entry
+               perform map-entry-size(j) times
+                   if map-entry-empty(j)
+                       set block-empty(i) to true
                    else
-                       add 1 to left-pointer
+                       set block-empty(i) to false
+                       move map-entry-file-id(j) to block-file-id(i)
+                   end-if
+                   set i up by 1
+                   set last-written-block to i
+               end-perform
+           end-perform
+           set last-written-block to i.
+           
+       compact-disk-fragmentation.
+           compute right-block = last-written-block - 1
+           move 1 to left-block
+           perform until right-block <= left-block
+               if block-empty(right-block)
+                   subtract 1 from right-block
+               else
+                   if block-empty(left-block)
+                       set block-empty(left-block) to false
+                       set block-empty(right-block) to true
+                       move block-file-id(right-block) to 
+                       block-file-id(left-block)
+                   else
+                       add 1 to left-block
                    end-if
                end-if
            end-perform.
        
+       compact-disk-whole-files.
+           compute right-map-entry = last-written-map-entry - 1
+           move 1 to left-map-entry
+           perform test after until current-file = 0
+               
+               move last-written-map-entry to right-map-entry
+               perform until 
+                 not map-entry-empty(right-map-entry) 
+                 and map-entry-file-id(right-map-entry)
+                   = current-file
+                   subtract 1 from right-map-entry
+               end-perform
+               move map-entry-size(right-map-entry) to current-file-size
+               
+               move 1 to left-map-entry
+               perform until 
+               map-entry-empty(left-map-entry) and 
+               map-entry-size(left-map-entry) >=
+               map-entry-size(right-map-entry)
+               or
+               left-map-entry = right-map-entry
+                   add 1 to left-map-entry
+               end-perform
+               
+               if left-map-entry < right-map-entry
+                   if map-entry-size(left-map-entry) > current-file-size
+                       move left-map-entry to insert-index
+                       perform shift-map
+                       add 1 to right-map-entry
+                       subtract map-entry-size(right-map-entry) from 
+                       map-entry-size(insert-index + 1)
+                   end-if
+                   move map-entry(right-map-entry) to 
+                   map-entry(left-map-entry)
+                   set map-entry-empty(right-map-entry) to true
+               end-if
+               
+               subtract 1 from current-file
+           end-perform.
+       
+       shift-map.
+           subtract 1 from last-written-map-entry
+           perform test after varying j from last-written-map-entry by 
+           -1 until j = insert-index
+               move map-entry(j) to map-entry(j + 1)
+           end-perform
+           set map-entry-empty(insert-index) to true
+           move 0 to map-entry-size(insert-index)
+           add 2 to last-written-map-entry.
+           
        checksum.
            perform varying i from 1 by 1 until i >= last-written-block
                if not block-empty(i)
@@ -109,6 +200,28 @@
                                            * block-file-id(i)
                end-if
            end-perform.
+       
+       display-disk-map.
+           display "Sizes:"
+           perform varying j from 1 by 1 until j >= 
+           last-written-map-entry
+               move map-entry-size(j) to display-number
+               display function trim(display-number) with no 
+               advancing
+           end-perform
+           display " "
+           display "IDs:"
+           perform varying j from 1 by 1 until j >= 
+           last-written-map-entry
+               if map-entry-empty(j)
+                   display "e" with no advancing
+               else
+                   move map-entry-file-id(j) to display-number
+                   display function trim(display-number) with no 
+                   advancing
+               end-if
+           end-perform
+           display " ".
            
        display-disk.
            perform varying i from 1 by 1 until i >= last-written-block
